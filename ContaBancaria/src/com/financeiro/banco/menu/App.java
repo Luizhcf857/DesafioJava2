13789e6d6cc53407311b1a734878cb9f830880c9
Luiz Henrique <luizhenrique3781@gmail.com>
2025-09-04 21:48:30 -0300

    Add files via upload
    Desafio2, foi adicionado em fase de teste

---
+++ ContaBancaria/src/com/financeiro/banco/menu/App.java
@@ -0,0 +1,14 @@
+package com.financeiro.banco.menu;
+
+import com.financeiro.controles.Controller;
+
+public class App {
+
+	public static void main(String[] args) {
+		Controller dados = new Controller();
+		
+		dados.sistema();
+
+	}
+
+}
